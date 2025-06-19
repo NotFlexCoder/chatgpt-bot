7ba8f183acf9d5c48a7223773a7f298ba3fdf3ec
Flex Coder <jaychonkar617@gmail.com>
2025-06-19 09:26:56 +0530

    index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post("/", async (req, res) => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           chat_id: chatId,
-          text: "👋 Hello there! I'm here to chat with you.\n\n💬 Just send me a message and I’ll try my best to respond.\n✨ Let’s get started!",
+          text: "👋 Hello there! I'm here to chat with you.\n\n💬 Just send me a message and I’ll try my best to respond.",
           reply_to_message_id: messageId
         }),
       });
